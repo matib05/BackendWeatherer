2b2ddc2beb2b9391437adff14936a499ea36232c
Muhammad Ahmad <mahmad@InComm.com>
2017-12-06 21:05:26 -0500

    incomplete

--- src/main/java/com/incomm/mahmad/Application.java
+++ src/main/java/com/incomm/mahmad/Application.java
@@ -1,5 +1,6 @@
 package com.incomm.mahmad;
 
+import com.incomm.mahmad.model.response.WeatherData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,28 +8,79 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner{
+public class Application implements CommandLineRunner {
 
+    @Autowired
+    static JdbcTemplate jdbcTemplate;
     private static final Logger log  = LoggerFactory.getLogger(Application.class);
+    static String city = "Atlanta";
+    private static final String APPID = "31576d96eb3acb7333670187bc45f085";
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        Timer timer = new Timer();
     }
-
-    @Autowired
-    JdbcTemplate jdbcTemplate;
 
     @Override
     public void run(String... strings) throws Exception {
 
         log.info("Creating tables");
         jdbcTemplate.execute("DROP TABLE weather IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE weather("
-                + "weatherData VARCHAR(700))");
+        jdbcTemplate.execute("CREATE TABLE weather(weatherData VARCHAR(700))");
+        log.info("calling api");
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runTimerTask();
+            }
+        };
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(timerTask, 0, 5*1000);
+    }
+
+
+    public static String getCityId() {
+        String cityId = "";
+        String fileName = "city_list.txt";
+        String content = null;
+        try {
+            content = new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert content != null;
+        String[] cont = content.split("\n");
+        for (int i = 0 ; i < cont.length; i++) {
+            if (cont[i].equalsIgnoreCase(city.toLowerCase())) {
+                cityId= cont[i].split(",")[0];
+                log.debug("cityId= " + cityId);
+            }
+        }
+        return cityId.trim();
+    }
+
+    public static void runTimerTask() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://api.openweathermap.org/data/2.5/weather?id=" + getCityId() + "&appid=" + APPID;
+        WeatherData weather = restTemplate.getForObject(url, WeatherData.class);
+        addToDatabase(weather.getWeather().get(0).getDescription(), weather.getName().trim(), weather.getMain().getTemp());
+        System.out.println(weather.toString());
+    }
+
+    public static void addToDatabase(String description, String city, Double temp) {
+        if (description == null || city == null) {
+            System.out.println("Null");
+            return;
+        }
+        String weatherData = description + " " + city + " " + temp;
+        System.out.println(jdbcTemplate.update("INSERT INTO weather(weatherData) VALUES (?)", weatherData));
     }
 }
--- src/main/java/com/incomm/mahmad/service/ApiCaller.java
+++ src/main/java/com/incomm/mahmad/service/ApiCaller.java
@@ -13,14 +13,16 @@ import java.util.TimerTask;
 public class ApiCaller extends TimerTask {
 
 
+    String city = "Atlanta";
 
     private static final String APPID = "31576d96eb3acb7333670187bc45f085";
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public void getCityId() {
-        String fileName = "C:\\Users\\mahmad\\IdeaProjects\\BackendWeatherer\\src\\main\\java\\com\\incomm\\mahmad\\city_list.txt";
+    public String getCityId() {
+        String cityId = "";
+        String fileName = "city_list.txt";
         String content = null;
         try {
             content = new String(Files.readAllBytes(Paths.get(fileName)));
@@ -29,16 +31,18 @@ public class ApiCaller extends TimerTask {
         }
         String[] cont = content.split("\n");
         for (int i = 0 ; i < cont.length; i++) {
-            String cityId  = cont[i].split(",")[0];
+            if (cont[i].equalsIgnoreCase(city.toLowerCase())) {
+                cityId= cont[i].split(",")[0];
+            }
         }
-
+        return cityId.trim();
     }
 
 
     @Override
     public void run() {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://api.openweathermap.org/data/2.5/weather?id=" ;//+ //city + "&appid=" + APPID;
+        String url = "http://api.openweathermap.org/data/2.5/weather?id=" + getCityId() + "&appid=" + APPID;
         WeatherData weather = restTemplate.getForObject(url, WeatherData.class);
         addToDatabase(weather.getWeather().get(0).getDescription(), weather.getName().toString().trim(), weather.getMain().getTemp());
         System.out.println(weather.toString());
